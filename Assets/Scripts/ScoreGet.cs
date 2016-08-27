a7d06bb5825a9097409198c5cdaf866f81c10af8
ほっしー <m011438553@edu.teu.ac.jp>
2016-08-28 01:30:21 +0900

    [修正]スポーン変更、敵の動き修正。

--- Assets/Scripts/ScoreGet.cs
+++ Assets/Scripts/ScoreGet.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class ScoreGet : MonoBehaviour {
     private ScoreManager score_manager;
     public int add_score = 100;
+    private Spawn spawn;
 	// Use this for initialization
 	void Start () {
         score_manager = GameObject.Find("GameController").GetComponent<ScoreManager>();
-	}
+        spawn = GameObject.Find("spawn_object").GetComponent<Spawn>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/UFO_move.cs
+++ Assets/Scripts/UFO_move.cs
@@ -11,22 +11,42 @@ public class UFO_move : MonoBehaviour {
     public float fuwa_add = 1.0f;
     public float fuwa_range = 2.0f;
     public float fuwa_speed = 0.1f;
+    private int fuwa_random = Random.Range(1, 3);
+    private Spawn spawn;
+    private float add_speed;
+    private float fuwa_add_speed;
     // Use this for initialization
     void Start()
     {
         pos = gameObject.transform.position;
         player = GameObject.FindGameObjectWithTag("Player");
+        spawn = GameObject.Find("spawn_object").GetComponent<Spawn>();
        // pos.z = 0;
         pos.y = player.transform.position.y;
         start_pos = pos;
+        add_speed = (spawn.spawn_count) / 30;
+        fuwa_add_speed = (spawn.spawn_count) / 15;
+        if (fuwa_random == 1)
+        {
+            fuwa_range *= -1;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.position = pos;
-        pos.z -= fuwa_speed;
-        fuwa += fuwa_add;
+       
+        pos.z -= (fuwa_speed + add_speed);
+        fuwa += (fuwa_add + fuwa_add_speed);
         pos.x = start_pos.x + (Mathf.Sin(Mathf.PI * 2 / fuwa_time * fuwa)) * fuwa_range;
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Death")
+        {
+            Destroy(gameObject);
+        }
     }
 }
