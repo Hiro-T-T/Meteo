94ee9254acb1aeed2e196c4efd06001ae2bff715
ほっしー <m011438553@edu.teu.ac.jp>
2016-08-27 17:05:05 +0900

    [修正][追加]playerMoveスクリプトの修正、spawnスクリプトの追加

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+    public GameObject enemy;    //敵オブジェクト
+    public Transform ground;    //地面オブジェクト
+    private float count = 5;     //一度に何体のオブジェクトをスポーンさせるか
+    private float interval = 5;  //何秒おきに敵を発生させるか
+    private float timer;        //経過時間
+                                // Use this for initialization
+    void Start()
+    {
+        Spawn_p();    //初期スポーン
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count = Random.Range(1, 5);
+        timer += Time.deltaTime;    //経過時間加算
+        if (timer >= interval)
+        {
+            Spawn_p();    //スポーン実行
+            interval = Random.Range(4, 8);
+            timer = 0;  //初期化
+        }
+    }
+    void Spawn_p()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            //スポーン座標指定
+            float x = Random.Range(-50f, 50f);
+            float y = Random.Range(0f, 100f);
+            float z = Random.Range(-50f, 50f);
+            //床から浮かしている
+            Vector3 pos = new Vector3(x, y, z) + ground.position;
+            //Enemt出現
+            GameObject.Instantiate(enemy, pos, Quaternion.identity);
+            Debug.Log("Spawn");//Debug
+        }
+
+    }
+}
--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -2,15 +2,14 @@
 using System.Collections;
 
 public class playerMove : MonoBehaviour {
-    Touch touch = Input.GetTouch(0);
-    Vector3 pos;
-	// Use this for initialization
-	void Start () {
+   // Touch touch = Input.GetTouch(0);
+    private Vector3 pos;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
         pos = gameObject.transform.position;
-        if (Input.touchSupported)
-        {
-            Debug.Log("ちん");
-        }
+
 	
 	}
 	
@@ -18,14 +17,27 @@ public class playerMove : MonoBehaviour {
 	void Update () {
 
         gameObject.transform.position = pos;
-
-	    if(Input.touchCount > 0)
+        if(Input.GetMouseButtonDown(0) == true)
         {
-            if(touch.phase == TouchPhase.Moved)
-            {
+            screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            pos.x = Input.mousePosition.x;
+            offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, screenPoint.z));
+
+        }
+        if(Input.GetMouseButton(0) == true)
+        {
+
+            pos.x = Input.mousePosition.x;
+            Vector3 currentScreenPoint = new Vector3(pos.x, pos.y, screenPoint.z);
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + offset;
+
+            transform.position = currentPosition;
+        }
+        
+            
                 //pos.x = touch.deltaPosition.x;
                 Debug.Log("タッチー");
-            }
-        }
+           
+        
 	}
 }
