cc876d5a448cfacea86b53d02cfd252297287d72
ほっしー <m011438553@edu.teu.ac.jp>
2016-08-28 02:35:45 +0900

    [追加]stageManagerの追加

---
+++ Assets/Scripts/stageManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stageManager : MonoBehaviour {
+    public float game_rotate = 0.0f;
+    private int rotate_time = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        game_rotate = (90 - game_rotate) / 30;
+	}
+}
