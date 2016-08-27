f7405c36a0011a3721ae07feccb05ee41ff18eb9
ほっしー <m011438553@edu.teu.ac.jp>
2016-08-27 15:48:24 +0900

    [追加]プレイヤーの動きのタッチ確認用スクリプト

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+    Touch touch = Input.GetTouch(0);
+    Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = gameObject.transform.position;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position = pos;
+
+	if(Input.touchCount > 0)
+        {
+            if(touch.phase == TouchPhase.Moved)
+            {
+                pos.x = touch.position.x;
+            }
+        }
+	}
+}
