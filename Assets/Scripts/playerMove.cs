1e8c97cec7253e15daf864f6ca940487f58b8fce
ほっしー <m011438553@edu.teu.ac.jp>
2016-08-27 18:54:44 +0900

    [追加][修正]敵の動き追加。プレイヤーの動き修正。

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -6,6 +6,8 @@ public class playerMove : MonoBehaviour {
     private Vector3 pos;
     private Vector3 screenPoint;
     private Vector3 offset;
+    public Vector3 currentPosition;
+    public float move_limited = 1.5f;
     // Use this for initialization
     void Start () {
         pos = gameObject.transform.position;
@@ -16,27 +18,42 @@ public class playerMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        gameObject.transform.position = pos;
+        //クリック時（タッチ時）
         if(Input.GetMouseButtonDown(0) == true)
         {
+            //screenPoint変数に現在のプレイヤーのワールド座標を保存
             screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            //変数posのx座標にマウスをクリックした座標を習得
             pos.x = Input.mousePosition.x;
+            //offset変数にクリック座標とプレイヤー座標のずれを代入、zは関係ないのでそのまま
             offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, screenPoint.z));
 
         }
+        //ドラッグ時（スライド時）
         if(Input.GetMouseButton(0) == true)
         {
 
             pos.x = Input.mousePosition.x;
+            //なんとなくこちらで指定
             Vector3 currentScreenPoint = new Vector3(pos.x, pos.y, screenPoint.z);
-            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + offset;
-
+            //offsetとのずれを加味して座標指定
+            currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + offset;
+            //動け動け(´◉◞౪◟◉)
             transform.position = currentPosition;
+
+            if(transform.position.x > move_limited)
+            {
+                transform.position = new Vector3(move_limited, pos.y, currentPosition.z);
+            }
+            if(transform.position.x < -move_limited)
+            {
+                transform.position = new Vector3(-move_limited, pos.y, currentPosition.z);
+            }
+
         }
-        
-            
-                //pos.x = touch.deltaPosition.x;
-                Debug.Log("タッチー");
+
+
+
            
         
 	}
