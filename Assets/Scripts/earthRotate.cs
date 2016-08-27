4432bab8c6bc1b066e03642663b80fbbb0bf63de
tadakun <kokoroko.arive@gmail.com>
2016-08-27 18:55:49 +0900

    [追加]earth・cloudオブジェクトの配置・実装

---
+++ Assets/Scripts/earthRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class earthRotate : MonoBehaviour {
+
+    public float earth_rotate = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(1, 0, 0), -earth_rotate);
+    }
+}
